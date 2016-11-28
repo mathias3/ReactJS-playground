052ed86a5cce99d824375550f47c53a351954978
mathias3 <maciej.osowski@gmail.com>
2016-11-28 14:02:12 +0000

    Create Greeting.js

---
+++ pass-prop-component-to-component/Greeting.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+
+var Greeting = React.createClass({
+  render: function () {
+    return <h1>Hi there, {this.props.name}!</h1>;
+  }
+});
+
+// ReactDOM.render goes here:
+module.exports = Greeting;
