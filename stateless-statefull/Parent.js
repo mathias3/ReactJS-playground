2a4530146d77eb8667816a4f232053abae3000a7
mathias3 <maciej.osowski@gmail.com>
2016-12-14 00:03:07 +0000

    Create Parent.js

---
+++ stateless-statefull/Parent.js
@@ -0,0 +1,13 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+
+var Parent = React.createClass({
+  getInitialState: function () {
+    return { name: 'Frarthur' };
+  },
+  
+  render: function () {
+    return <div></div>;
+  }
+});
