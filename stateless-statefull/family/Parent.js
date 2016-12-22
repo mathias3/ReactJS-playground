39300855b425cb5830f2a127f844a4b70f2cadb9
mathias3 <maciej.osowski@gmail.com>
2016-12-22 23:12:06 +0100

    Create Parent.js

---
+++ stateless-statefull/family/Parent.js
@@ -0,0 +1,32 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Child = require('./Child');
+var Sibling = require('./Sibling');
+
+var Parent = React.createClass({
+  getInitialState: function () {
+    return { name: 'Frarthur' };
+  },
+  
+  changeName: function (newName) {
+    this.setState({
+      name: newName
+    });
+  },
+
+  render: function () {
+    return (
+      <div>
+        <Child 
+          name={this.state.name} 
+          onChange={this.changeName} />
+        <Sibling name={this.state.name} />
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <Parent />,
+  document.getElementById('app')
+);
