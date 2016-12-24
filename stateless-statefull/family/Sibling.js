ff134e33dc9d281bc2d303aefad2e029b80572d4
mathias3 <maciej.osowski@gmail.com>
2016-12-24 01:37:36 +0100

    Update Sibling.js

--- stateless-statefull/family/Sibling.js
+++ stateless-statefull/family/Sibling.js
@@ -5,9 +5,9 @@ var Sibling = React.createClass({
 var name = this.props.name;
     return (
       <div>
-        <h1>Hey, my name is Frarthur!</h1>
-        <h2>Don't you think Frarthur is the prettiest name ever?</h2>
-        <h2>Sure am glad that my parents picked Frarthur!</h2>
+        <h1>Hey, my name is {name}!</h1>
+        <h2>Don't you think {name} is the prettiest name ever?</h2>
+        <h2>Sure am glad that my parents picked {name}!</h2>
       </div>
     );
   }
