3e1e4ddc1910425ab1e830e84f0c6a94a0e6f0f2
mathias3 <maciej.osowski@gmail.com>
2016-12-25 20:10:19 +0100

    Create  styleMs.js

---
+++ styling/ styleMs.js
@@ -0,0 +1,9 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var styleMe = <h1>Please style me!  I am so bland!</h1>;
+
+ReactDOM.render(
+	styleMe, 
+	document.getElementById('app')
+);
