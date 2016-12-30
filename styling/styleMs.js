8a1e2aa6dd246de25a1c1a01cebd8c6d355eb836
mathias3 <maciej.osowski@gmail.com>
2016-12-30 01:09:11 +0100

    Update styleMs.js

--- styling/styleMs.js
+++ styling/styleMs.js
@@ -4,7 +4,7 @@ var styles = {
   background: 'lightblue',
   color:      'darkred'
 }
-var styleMe = <h1 style={{ background: 'lightblue', color: 'darkred' }}>Please style me!  I am so bland!</h1>;
+var styleMe = <h1 style={styles}>Please style me!  I am so bland!</h1>;
 
 ReactDOM.render(
 	styleMe, 
