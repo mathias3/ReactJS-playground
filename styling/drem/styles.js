009374d64101a261b2686f7dbd374745d0ab17c7
mathias3 <maciej.osowski@gmail.com>
2017-01-03 23:01:59 +0100

    Create styles.js

---
+++ styling/drem/styles.js
@@ -0,0 +1,5 @@
+var fontFamily = 'Comic Sans MS, Lucida Handwriting, cursive';
+var background = 'pink url("https://media.giphy.com/media/oyr89uTOBNVbG/giphy.gif") fixed';
+var fontSize   = '4em';
+var padding = '45px 0';
+var color  = 'green';
