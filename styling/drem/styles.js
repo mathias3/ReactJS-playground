b3218fbfc456d72d6297a21566ee89551ca903bb
mathias3 <maciej.osowski@gmail.com>
2017-01-04 23:45:20 +0100

    Update styles.js

--- styling/drem/styles.js
+++ styling/drem/styles.js
@@ -3,3 +3,11 @@ var background = 'pink url("https://media.giphy.com/media/oyr89uTOBNVbG/giphy.gi
 var fontSize   = '4em';
 var padding = '45px 0';
 var color  = 'green';
+
+module.exports = {
+  fontFamily: fontFamily,
+  background: background,
+  fontSize:   fontSize,
+  padding:    padding,
+  color:      color
+};
